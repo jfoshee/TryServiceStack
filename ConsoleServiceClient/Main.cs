97b8768b344763c2630d6d8c67fa89b206007aae
Jacob Foshee <jacobf@gmail.com>
2012-10-19 14:52:38 -0400

    Can use base address
    ServiceStack can infer path by its own conventions

--- ConsoleServiceClient/Main.cs
+++ ConsoleServiceClient/Main.cs
@@ -10,7 +10,7 @@ namespace ConsoleServiceClient
         {
             Console.Write("What is your name? ");
             var name = Console.ReadLine();
-            var client = new JsonServiceClient("http://127.0.0.1:8080/servicestack/json/syncreply/Hello");
+            var client = new JsonServiceClient("http://127.0.0.1:8080");
             var response = client.Send<HelloResponse>(new Hello { Name = name });
             Console.WriteLine(response.Result);
         }
