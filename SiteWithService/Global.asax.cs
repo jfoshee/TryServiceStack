1838201c33f91389bacf7b545d8ad83ed66f7933
Jacob Foshee <jacobf@gmail.com>
2012-10-18 23:36:53 -0400

    Added ServiceStack references to MVC SiteWithService

--- SiteWithService/Global.asax.cs
+++ SiteWithService/Global.asax.cs
@@ -1,9 +1,3 @@
-
-﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
@@ -14,13 +8,12 @@ namespace SiteWithService
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            routes.IgnoreRoute("servicestack/{*pathInfo}");
             routes.MapRoute(
                 "Default",
                 "{controller}/{action}/{id}",
                 new { controller = "Home", action = "Index", id = "" }
             );
-
         }
 
         protected void Application_Start()
