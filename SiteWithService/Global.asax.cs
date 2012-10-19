cc21d8047762e8a5e5629a1bf16919f2afd3b37b
Jacob Foshee <jacobf@gmail.com>
2012-10-18 23:43:41 -0400

    Got both service and html being served!
    http://127.0.0.1:8080/servicestack/hello/world?format=json

--- SiteWithService/Global.asax.cs
+++ SiteWithService/Global.asax.cs
@@ -1,5 +1,8 @@
 using System.Web.Mvc;
 using System.Web.Routing;
+using ServiceStack.WebHost.Endpoints;
+using Funq;
+using TryServiceStack;
 
 namespace SiteWithService
 {
@@ -20,6 +23,24 @@ namespace SiteWithService
         {
             AreaRegistration.RegisterAllAreas();
             RegisterRoutes(RouteTable.Routes);
+            new HelloAppHost().Init();
         }
+
+        public class HelloAppHost : AppHostBase
+        {
+            //Tell Service Stack the name of your application and where to find your web services
+            public HelloAppHost() : base("Hello Web Services", typeof(HelloService).Assembly)
+            {
+            }
+            
+            public override void Configure(Container container)
+            {
+                //register user-defined REST-ful urls
+                Routes
+                    .Add<Hello>("/hello")
+                        .Add<Hello>("/hello/{Name}")
+                        .Add<AddRequest>("/add");
+            }
+        } 
     }
 }
